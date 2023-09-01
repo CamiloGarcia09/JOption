7ff4324918fa41262028ac865938b2128144f51a
Camilo Garcia <juan092005ca@gmail.com>
2023-08-31 22:58:45 -0500

    TallerN°1

--- src/com/Bicicleta/App/AppBicileta.java
+++
@@ -1,5 +0,0 @@
-package com.Bicicleta.App;
-
-public class AppBicileta {
-
-}
--- src/com/CuentaBancaria/Dominio/Usuario.java
+++
@@ -1,7 +0,0 @@
-package com.CuentaBancaria.Dominio;
-
-public class Usuario {
-    public String name;
-    public String identification;
-}
-
